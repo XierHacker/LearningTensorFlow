f75d0ac7c9df04fa094cba36c5e06530ccf3552f
xierhacker <xierhacker@163.com>
2017-05-04 18:22:14 +0800

    add ipynb file

---
+++ Mnist/data.py
@@ -0,0 +1,9 @@
+from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow as tf
+import numpy as np
+
+#load data
+mnist=input_data.read_data_sets("MNIST_data/",one_hot=True)
+
+#print(mnist.train.images.shape,mnist.train.labels.shape)
+#print(mnist.train.images[0])
