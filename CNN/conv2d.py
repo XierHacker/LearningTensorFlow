3eb79238d3d312dafbc3518fae5b217931ec4608
XierHacker <xierhacker@163.com>
2017-05-13 10:39:03 +0800

    initial commit

--- CNN/conv2d.py
+++ CNN/conv2d.py
@@ -29,6 +29,7 @@ W=np.array([[[[1,-1,0],[1,0,1],[-1,-1,0]],
 #print(x[0][:,:,0])
 '''
 
+#this
 x=np.reshape(a=x,newshape=(1,7,7,3))
 
 #w
@@ -42,15 +43,20 @@ print(W[1][:,:,0])
 print(W[1][:,:,1])
 print(W[1][:,:,2])
 '''
-W=np.reshape(a=W,newshape=(3,3,3,2))
+
+#this can make the W'th format match the fuction's
+W=np.transpose(W,axes=(1,2,3,0))
 print(W.shape)
 
+#
+b=np.array([1,0])
 #define graph
 graph=tf.Graph()
 with graph.as_default():
    input=tf.constant(value=x,dtype=tf.float32,name="input")
    filter=tf.constant(value=W,dtype=tf.float32,name="filter")
-   result=tf.nn.conv2d(input=input,filter=filter,strides=[1,2,2,1],padding="VALID",name="conv")
+   bias=tf.constant(value=b,dtype=tf.float32,name="bias")
+   result=tf.nn.conv2d(input=input,filter=filter,strides=[1,2,2,1],padding="VALID",name="conv")+bias
 
 with tf.Session(graph=graph) as sess:
     r=sess.run(result)
