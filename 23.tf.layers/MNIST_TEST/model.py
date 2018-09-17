a4b95ea7e30eceb7a31abb8691a00d6e7ae1a9a9
XierHacker <xierhacker@163.com>
2018-09-17 19:58:18 +0800

    add tf.layers

--- 23.tf.layers/MNIST_TEST/model.py
+++ 23.tf.layers/MNIST_TEST/model.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
 import parameter
+from tensorflow.contrib.layers import xavier_initializer
 
 class Model():
     def __init__(self):
@@ -10,7 +11,7 @@ class Model():
         #---------------------------------------conv1--------------------------------------#
         logits_conv1=tf.layers.conv2d(
             inputs=input,filters=32,kernel_size=5,strides=1,padding="SAME",activation=tf.nn.relu,
-            kernel_regularizer=regularizer,name="conv_1"
+            kernel_initializer=xavier_initializer(),kernel_regularizer=regularizer,name="conv_1"
         )
         print("shape of logits_conv1:",logits_conv1.shape)
         logits_conv1=tf.layers.max_pooling2d(inputs=logits_conv1,pool_size=2,strides=2,padding="SAME",name="padded_conv1")
@@ -19,17 +20,18 @@ class Model():
         # ---------------------------------------conv2--------------------------------------#
         logits_conv2 = tf.layers.conv2d(
             inputs=logits_conv1, filters=64, kernel_size=5, strides=1, padding="SAME", activation=tf.nn.relu,
-            kernel_regularizer=regularizer, name="conv_2"
+            kernel_initializer=xavier_initializer(),kernel_regularizer=regularizer, name="conv_2"
         )
         print("shape of logits_conv2:", logits_conv2.shape)
-        logits_conv2 = tf.layers.max_pooling2d(inputs=logits_conv2, pool_size=2, strides=2, padding="SAME",
-                                               name="padded_conv2")
+        logits_conv2 = tf.layers.max_pooling2d(inputs=logits_conv2, pool_size=2, strides=2, padding="SAME",name="padded_conv2")
         print("shape of logits_conv2:", logits_conv2.shape)
 
         logits_flatten=tf.layers.flatten(inputs=logits_conv2,name="logits_flatten")
         print("shape of logits_flatten:", logits_flatten.shape)
 
-        logits=tf.layers.dense(inputs=logits_flatten,units=10,activation=tf.nn.relu,kernel_regularizer=regularizer,
+        logits=tf.layers.dense(inputs=logits_flatten,units=10,activation=tf.nn.relu,
+                               kernel_initializer=xavier_initializer(),
+                               kernel_regularizer=regularizer,
                                name="logits")
         print("shape of logits:", logits.shape)
         return logits
