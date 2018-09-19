8a75bee9a85e1fff6fee1051bc0d455f6506df61
XierHacker <xierhacker@163.com>
2018-09-19 17:24:17 +0800

    add tf.layers

--- 23.tf.layers/MNIST_TEST/model.py
+++ 23.tf.layers/MNIST_TEST/model.py
@@ -10,7 +10,7 @@ class Model():
     def forward(self,input,regularizer):
         #---------------------------------------conv1--------------------------------------#
         logits_conv1=tf.layers.conv2d(
-            inputs=input,filters=32,kernel_size=5,strides=1,padding="SAME",activation=tf.nn.relu,
+            inputs=input,filters=32,kernel_size=5,strides=1,padding="SAME",activation=None,
             kernel_initializer=xavier_initializer(),kernel_regularizer=regularizer,name="conv_1"
         )
         print("shape of logits_conv1:",logits_conv1.shape)
@@ -19,7 +19,7 @@ class Model():
 
         # ---------------------------------------conv2--------------------------------------#
         logits_conv2 = tf.layers.conv2d(
-            inputs=logits_conv1, filters=64, kernel_size=5, strides=1, padding="SAME", activation=tf.nn.relu,
+            inputs=logits_conv1, filters=64, kernel_size=5, strides=1, padding="SAME", activation=None,
             kernel_initializer=xavier_initializer(),kernel_regularizer=regularizer, name="conv_2"
         )
         print("shape of logits_conv2:", logits_conv2.shape)
@@ -29,7 +29,7 @@ class Model():
         logits_flatten=tf.layers.flatten(inputs=logits_conv2,name="logits_flatten")
         print("shape of logits_flatten:", logits_flatten.shape)
 
-        logits=tf.layers.dense(inputs=logits_flatten,units=10,activation=tf.nn.relu,
+        logits=tf.layers.dense(inputs=logits_flatten,units=10,activation=None,
                                kernel_initializer=xavier_initializer(),
                                kernel_regularizer=regularizer,
                                name="logits")
