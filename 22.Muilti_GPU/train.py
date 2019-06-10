2a01046bc09eeb570d1906b48707173ef1fa3651
xierhacker <xierhacker@163.com>
2019-06-10 20:26:08 +0800

    update to tensorflow 2.x

--- 22.Muilti_GPU/train.py
+++ 22.Muilti_GPU/train.py
@@ -7,6 +7,13 @@ import model
 
 
 DEVICES_LIST=["gpu:4","gpu:5","gpu:6"]
+#parameters
+BATCH_SIZE=2
+CLASS_NUM=10
+EPOCHS=10
+
+
+
 
 
 print(tf.__version__)
@@ -15,10 +22,14 @@ print(tf.__version__)
 
 #expand dim to use convlution 2D
 x_train=np.expand_dims(a=x_train,axis=-1)/np.float32(255)
-# print("x_train:\n",x_train)
+train_dataset=tf.data.Dataset.from_tensor_slices((x_train,y_train)).batch(BATCH_SIZE)
+# for records in train_dataset:
+#     print("records:\n",records[0])
+#     print("records:\n",records[1])
+    
 
 x_test=np.expand_dims(a=x_test,axis=-1)/np.float32(255)
-# print("x_test:\n",x_test)
+test_dataset=tf.data.Dataset.from_tensor_slices((x_test,y_test))
 
 #
 # plt.imshow(X=x_train[1,:,:,0])
@@ -31,6 +42,14 @@ def train_step():
 def train():
     strategy=tf.distribute.MirroredStrategy(devices=DEVICES_LIST)
     print("num devices:",strategy.num_replicas_in_sync)
+    with strategy.scope():
+        train_dist_dataset=strategy.experimental_distribute_dataset(train_dataset)
+        for records in train_dist_dataset:
+            print("records:\n",records[0])
+            print("records:\n",records[1])
+            
+
+
     
 
 
