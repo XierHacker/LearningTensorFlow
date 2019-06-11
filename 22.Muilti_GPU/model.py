0c7d4eccfce837743a48ed09b58634be25ce087c
xierhacker <xierhacker@163.com>
2019-06-11 11:09:52 +0800

    update to tensorflow 2.x

--- 22.Muilti_GPU/model.py
+++ 22.Muilti_GPU/model.py
@@ -5,10 +5,32 @@ import tensorflow as tf
 class CNN(tf.keras.Model):
     def __init__(self,filters,kernel_size,strides,activation):
         super(CNN,self).__init__()
-        conv1=tf.keras.layers.Conv2D(filters=filters,kernel_size=kernel_size,strides=strides,activation=activation)
-        max_pool1=tf.keras.layers.MaxPooling2D()
-        
+        self.conv1=tf.keras.layers.Conv2D(filters=filters,kernel_size=kernel_size,strides=strides,activation=activation)
+        self.max_pool1=tf.keras.layers.MaxPooling2D()
+        self.conv2=tf.keras.layers.Conv2D(filters=filters,kernel_size=kernel_size,strides=strides,activation=activation)
+        self.max_pool2=tf.keras.layers.MaxPooling2D()
+        self.flatten=tf.keras.layers.Flatten()
+        self.fc1=tf.keras.layers.Dense(units=filters,activation=activation)
+        self.fc2=tf.keras.layers.Dense(units=10,activation=None)
     
-    def __call__():
-        pass
+    def __call__(self,x):
+        x=self.conv1(x)
+        x=self.max_pool1(x)
+        x=self.conv2(x)
+        x=self.max_pool2(x)
+        x=self.flatten(x)
+        x=self.fc1(x)
+        x=self.fc2(x)
+        #print("x.shape",x)
+        return x
 
+
+
+
+if __name__=="__main__":
+    cnn=CNN(filters=64,kernel_size=3,strides=1,activation=tf.nn.relu)
+
+    inputs=tf.ones(shape=(30,28,28,3),dtype=tf.float32)
+
+    cnn(inputs)
+    
--- 22.Muilti_GPU/train.py
+++ 22.Muilti_GPU/train.py
@@ -11,15 +11,16 @@ strategy=tf.distribute.MirroredStrategy()
 print("num devices:",strategy.num_replicas_in_sync)
 
 #parameters
-BATCH_SIZE_PER_REPLLICA=4
-BATCH_SIZE=BATCH_SIZE_PER_REPLLICA*strategy.num_replicas_in_sync
-print("batch_size_per_replica:",BATCH_SIZE_PER_REPLLICA)
+BATCH_SIZE_PER_REPLICA=4
+BATCH_SIZE=BATCH_SIZE_PER_REPLICA*strategy.num_replicas_in_sync
+print("batch_size_per_replica:",BATCH_SIZE_PER_REPLICA)
 print("batch_size:",BATCH_SIZE)
 
 CLASS_NUM=10
 EPOCHS=10
 
-
+#model save path
+CHECK_POINTS_PATH="./checkpoints/cnn"
 
 
 
@@ -47,15 +48,58 @@ def train_step():
     pass
 
 def train():
+    #trans dataset to distribute dataset
     with strategy.scope():
         train_dist_dataset=strategy.experimental_distribute_dataset(train_dataset)
-        for records in train_dist_dataset:
-            print("records:\n",records[0])
-            print("records:\n",records[1])
-            
+        test_dist_dataset=strategy.experimental_distribute_dataset(test_dataset)
 
-
+    #define loss
+    with strategy.scope():
+        SCC=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True,reduction=tf.keras.losses.Reduction.None)
+        def compute_loss(labels,predictions):
+            device_losses=SCC(labels,predictions)
+            print("device_loss")
+            device_loss=tf.nn.compute_average_loss(per_example_loss=device_losses,global_batch_size=BATCH_SIZE)
+            return device_loss
     
+    #define metrics
+
+    # model and optimizer must be created under `strategy.scope`.
+    with strategy.scope():
+        cnn=model.CNN(filters=64,kernel_size=3,strides=1,activation=tf.nn.relu)
+        #model = create_model()
+        optimizer=tf.keras.optimizers.Adam(learning_rate=0.001)
+        checkpoint = tf.train.Checkpoint(optimizer=optimizer, model=cnn)
+
+    #basic train step in one device
+    with strategy.scope():
+        def train_step(inputs):
+            x,y=inputs
+            with tf.GradientTape() as tape:
+                logits=cnn(x)
+                loss=compute_loss(labels=y,predictions=logits)
+            
+            gradients=tape.gradient(target=loss,sources=cnn.trainable_variables)
+            optimizer.apply_gradients(zip(gradients,cnn.trainable_variables))
+            return loss
+    
+    #distribute train_step use basic train step
+    with strategy.scope():
+        def dist_train_step(dataset_inputs):
+            replica_losses=strategy.experimental_run_v2(fn=train_step,args=(dataset_inputs,))
+            print("replica_losses:\n",replica_losses)
+            return strategy.reduce(reduce_op=tf.distribute.ReduceOp.SUM,value=replica_losses,axis=None)
+
+    for epoch in range(EPOCHS):
+        epoch_loss=0.0
+        num_batchs=0
+        for records in train_dist_dataset:
+            epoch_loss+=dist_train_step(records)
+            num_batchs+=1
+        epoch_loss=epoch_loss/num_batchs
+
+        print("epoch_loss:",epoch_loss)
+
 
 
 
